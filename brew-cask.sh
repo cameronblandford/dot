a06fa77b30b3e3c09b66a2a8ad399091b5e2c11c
Cameron Blandford <cam.blandford@gmail.com>
2019-04-02 11:56:19 -0400

    Add r stuff to cask script

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,7 +1,16 @@
 brew install caskroom/cask/brew-cask
+export HOMEBREW_CASK_OPTS="--appdir=/Applications --fontdir=/Library/Fonts"
 brew cask install alfred
 brew cask install spotify
 brew cask install iterm2
 brew cask install vlc
 brew cask install disk-inventory-x
 brew cask install gpgtools
+
+# install everything for R:
+
+brew cask install xquartz
+brew tap homebrew/science
+brew install --with-x11 r
+brew cask install --appdir=/Applications rstudio
+
